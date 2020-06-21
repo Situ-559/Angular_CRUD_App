980f7f68e38e6a61f7082ddd25ebff47eec0041b
Sourav Dash <dashsitu755@gmail.com>
2020-06-21 01:15:48 -0700

    Create Idepartment.model.ts

---
+++ src/app/Models/Idepartment.model.ts
@@ -0,0 +1,15 @@
+Skip to content
+Search or jump to…
+
+
+Angular_CRUD_App/src/app/Models/Idepartment.model.ts /
+@mrugankray mrugankray first commit
+9f944a1 on Sep 17, 2018
+4 lines (4 sloc)  59 Bytes
+  
+export class IDepartment {
+  id: number;
+  name: string;
+}
+
+
